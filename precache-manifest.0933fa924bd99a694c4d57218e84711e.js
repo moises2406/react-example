12af9484fe5dbc2d67b34fb882825e1e6f8dfe00
moises <moisesjusto2407@gmail.com>
2020-09-21 18:41:39 -0400

    Updates

---
+++ precache-manifest.0933fa924bd99a694c4d57218e84711e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e339757fd5fbffca9b66335df45daf76",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "f8faaa9275bba15793cf",
+    "url": "/react-example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5567f761d182c6018d6",
+    "url": "/react-example/static/js/2.96b30471.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-example/static/js/2.96b30471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8faaa9275bba15793cf",
+    "url": "/react-example/static/js/main.3d15b31d.chunk.js"
+  },
+  {
+    "revision": "591a35e8bba386688ec8",
+    "url": "/react-example/static/js/runtime-main.c33dc506.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
