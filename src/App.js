15eeeb7aa3f34ee2501f68d7da30e1d63c9036b4
moises <moisesjusto2407@gmail.com>
2020-09-23 13:58:49 -0400

    cal

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Navegasiones/>
 
         <div className="container">
-          <Route path='/' exact component={Viernes} />
+          <Route  exact component={Viernes} />
           <Route path="/Lunes" component={Lunes} />
           <Route path="/Sabados" component={Sabados} />
           <Route path="/Datos" component={Datos} />
